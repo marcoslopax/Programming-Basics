b40f1d61dbf7f8021d3f431637131a14abc29c58
Marcos Lopes <92733458+marcoslopax@users.noreply.github.com>
2022-07-21 17:22:02 -0300

    Update Criaecopia.java

--- Criaecopia.java
+++ Criaecopia.java
@@ -1,3 +1,5 @@
+//  "Criaecopia.java" was made to create a txt file lines witch the number lines is determinated by de user, also this code make copy of another txt file.
+
 package ProgramCopiar;
 
 import java.io.File;
